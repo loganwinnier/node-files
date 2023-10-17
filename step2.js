5bbc4faa0a7212ae35793c1c90f3435f3b32c949
Celeste Kilgore <celestekilgore@gmail.com>
2023-10-17 14:48:43 -0400

    working on webCat

--- step2.js
+++ step2.js
@@ -1,14 +1,32 @@
-'use strict'
+'use strict';
 
 const fsP = require("fs/promises");
 const argv = process.argv;
-/** cat: takes a path and reads the file with that path. Prints the content of that file */
+
+
+/** cat: takes a path and reads the file with that path.
+ * Prints the content of that file */
 async function cat(path) {
     try {
-        let contents = await fsP.readFile(path, "utf8")
-        console.log(contents)
+        let contents = await fsP.readFile(path, "utf8");
+        console.log(contents);
     } catch (err) {
-        console.log(`Error reading ${path}`, " Error:", err.message)
+        console.log(`Error reading ${path}`, " Error:", err.message);
         process.exit(1);
     }
 }
+
+/** webCat: takes a URL, reads the URL contents,
+ * and prints the contents to the console.*/
+async function webCat(url) {
+    let address = new URL(url);
+    try {
+        let response = await fetch(address);
+        let data = await fsP.readFile(await response, "utf8");
+    } catch (err) {
+        console.log(`Error fetching ${address}:`, " Error:", err.message);
+        process.exit(1);
+    }
+}
+
+webCat(argv[2]);
